919f750f172a78851a6194804387bef64c297509
RomanSerhieiev <unique.rmn.8@gmail.com>
2024-07-19 17:42:07 +0300

    controlled

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,46 @@
-import React, { FC } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
+
+interface IForm {
+    username: string,
+    password: string,
+}
 
 const Form: FC = () => {
+    const [formState, setFormState] = useState<IForm>({
+        username: 'asd',
+        password: 'qwe'
+    });
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        let user = {
+            username: formState.username,
+            password: formState.password
+        }
+        console.log(user);
+    };
+
+    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+        const input = e.target as HTMLFormElement;
+        setFormState({...formState, [input.name]: input.value})
+    }
+
+    // const handleUsernameChange = (e: FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLFormElement;
+    //     setFormState({...formState, username: input.value})
+    // }
+    //
+    // const handlePaswordChange = (e: FormEvent<HTMLInputElement>) => {
+    //     const input = e.target as HTMLFormElement;
+    //     setFormState({...formState, password: input.value})
+    // }
+
     return (
         <div>
-            <form>
-                <input type="text" name={'username'} placeholder={'Name'} />
-                <input type="text" name={'password'} placeholder={'Password'} />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name={'username'} placeholder={'Name'} value={formState.username} onChange={handleInputChange} />
+                <input type="text" name={'password'} placeholder={'Password'} value={formState.password} onChange={handleInputChange} />
+                <button>send</button>
             </form>
         </div>
     );
